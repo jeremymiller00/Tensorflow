2f2a19acfdd1c0c01dfa89f4b3c5304017c4ee78
Jeremy Miller <jeremymiller00@gmail.com>
2019-07-09 14:48:12 -0700

    add stanford lecture practice

--- src/stanford_lecture_practice.py
+++ src/stanford_lecture_practice.py
@@ -1,2 +1,14 @@
 import tensorflow as tf 
 
+# bias weights
+b= tf.Variable(tf.zeros((100,)))
+
+# weight matrix
+W = tf.Variable(tf.random_uniform((784, 100), -1, 1))
+
+
+# placeholder for data
+x = tf.placeholder(tf.float32, (100, 784))
+
+# graph
+h = tf.nn.relu(tf.matmul(x, W) + b)
